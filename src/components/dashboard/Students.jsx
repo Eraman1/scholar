0199f37cf35adec9edbe15e1a01e0db37e6a510d
Eraman1 <er.aman614@gmail.com>
2025-08-14 12:53:16 +0530

    fix

--- src/components/dashboard/Students.jsx
+++ src/components/dashboard/Students.jsx
@@ -1,6 +1,6 @@
 import { Download, Upload } from "lucide-react";
 import { useState, useMemo } from "react";
-import { downloadStudentData,applyBulk } from "../../api/studentApi";
+import { downloadStudentData, applyBulk } from "../../api/studentApi";
 import { saveAs } from "file-saver";
 
 export default function StudentCardTable() {
@@ -193,7 +193,7 @@ export default function StudentCardTable() {
 
   const studentHandleDownload = () => {
     window.open(
-      `http://localhost:8000/api/vedubuildApply/download-data`,
+      `https://api.vedubuild.org/api/vedubuildApply/download-data`,
       "_blank"
     );
   };
@@ -455,15 +455,14 @@ export default function StudentCardTable() {
                 </td>
                 <td className="px-4 py-3 text-sm whitespace-nowrap">
                   <span
-                    className={`px-2 py-1 rounded-full text-xs ${
-                      student.scholarship === "Merit-based"
-                        ? "bg-green-100 text-green-800"
-                        : student.scholarship === "Need-based"
+                    className={`px-2 py-1 rounded-full text-xs ${student.scholarship === "Merit-based"
+                      ? "bg-green-100 text-green-800"
+                      : student.scholarship === "Need-based"
                         ? "bg-blue-100 text-blue-800"
                         : student.scholarship === "Sports"
-                        ? "bg-purple-100 text-purple-800"
-                        : "bg-gray-100 text-gray-800"
-                    }`}
+                          ? "bg-purple-100 text-purple-800"
+                          : "bg-gray-100 text-gray-800"
+                      }`}
                   >
                     {student.scholarship}
                   </span>
